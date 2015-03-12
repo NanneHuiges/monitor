b725170cecd9cd9a72d90fc02c92e49ae3ec4ead
NanneHuiges <nanne@huiges.nl>
2015-03-12 21:35:13 +0100

    Bonus update

--- app/bin/import.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once '../vendor/autoload.php';
-
-$reader   = new \Monitor\Import\Reader('../test-01.csv');
-$inserter = new \Monitor\Import\Inserter($reader);
-$inserter->insert();
